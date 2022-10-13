9ac4331c5949774d8ef37e80422b3642592ad556
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-10-13 08:30:53 +0000

    Use pyproject.toml for all project metadata
    Remove boilerplate removed tests
    
    Remove _version_git
    
    Use version from package metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
-import glob
-import importlib.util
 from sysconfig import get_paths
 
 from numpy import get_include
 from numpy.distutils.core import Extension, setup
-
-# Import <package>._version_git.py without importing <package>
-path = glob.glob(__file__.replace("setup.py", "src/*/_version_git.py"))[0]
-spec = importlib.util.spec_from_file_location("_version_git", path)
-assert spec and spec.loader
-vg = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(vg)
 
 ext_modules = [
     Extension(
@@ -24,4 +15,4 @@ ext_modules = [
     ),
 ]
 
-setup(cmdclass=vg.get_cmdclass(), version=vg.__version__, ext_modules=ext_modules)
+setup(ext_modules=ext_modules)
